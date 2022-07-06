5f2627aa781b78a46d128acbe940fa3034918bf5
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-06 02:00:06 -0300

    structures conditional exercises

---
+++ EC_Ex07.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EC_Ex07 {
+    internal class Program {
+        static void Main(string[] args) {
+            double x, y;
+            Console.WriteLine("Inser the vallue to x:");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inser the vallue to y:");
+            y = double.Parse(Console.ReadLine());
+            if (x == 0 && y == 0) {
+                Console.WriteLine("Origin");
+            }
+            else if (x > 0 && y > 0) {
+                Console.WriteLine("Q1");
+            }
+            else if (x > 0 && y == 0) {
+                Console.WriteLine("X axis");
+            }
+            else if (y > 0 && x == 0) {
+                Console.WriteLine("Y axis");
+            }
+            else if (x > 0 && y < 0) {
+                Console.WriteLine("Q4");
+            }
+            else if (x < 0 && y > 0) {
+                Console.WriteLine("Q2");
+            }
+            else if(x < 0 && y < 0) {
+                Console.WriteLine("Q3");
+            }
+        }
+    }
+}
