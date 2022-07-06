5da91ca68ce942162709ad5fdf206f8041771696
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-06 02:04:52 -0300

    sequencial structure exercises

---
+++ ES_Ex02.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ES_Ex02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Informe o raio do circulo:");
+            double r = double.Parse(Console.ReadLine());
+            double area;
+            area = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"O valor do raio foi {r} e calculando a area a partir disso é de {area:F4}");
+
+        }
+    }
+}
