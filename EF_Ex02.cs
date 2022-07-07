b3458ff333ecb5c95a0df4195823b3223668905c
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-06 22:33:35 -0300

    Add files via upload

---
+++ EF_Ex02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EF_Ex02 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, x, i;
+            int contIn = 0, contOut = 0;
+            Console.WriteLine("Insert how many times x will be readed");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++) {
+                Console.WriteLine("Inesert x vallue:");
+                x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    contIn++;
+                }
+                else {
+                    contOut++;
+                }
+            }
+            Console.WriteLine($"In:{contIn}");
+            Console.WriteLine($"Out:{contOut}");
+        }
+    }
+}
