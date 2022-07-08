6b92dfae58b9874d453fa3aef9e998a5101fa3ab
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-07 22:32:23 -0300

    structure exercises for

---
+++ EF_Ex07.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EF_Ex07 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n, i;
+            Console.WriteLine("Insert a number:");
+            n = int.Parse(Console.ReadLine());
+            for(i = 1; i <= n; i++) {
+                Console.WriteLine($"{i} {Math.Pow(i,2)} {Math.Pow(i,3)}");
+            }
+        }
+    }
+}
