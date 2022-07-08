b9b2d6c1b2a287a4f95ee3690079ffaa58bc3dc0
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-08 11:33:48 -0300

    Structure exercises while

---
+++ While exercises/EW_Ex01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EW_Ex01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Password:");
+            int password = int.Parse(Console.ReadLine());
+            while (password != 2002) {
+                Console.WriteLine("Incorrect password!");
+                password = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Allowed acess!");
+        }
+    }
+}
