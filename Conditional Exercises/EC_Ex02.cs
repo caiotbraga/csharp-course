c02d20f44959fd587b179e09bcd4413b8ede88b3
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-08 01:28:51 -0300

    Add files via upload

---
+++ Conditional Exercises/EC_Ex02.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EC_Ex02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insert a number:");
+            int number = int.Parse(Console.ReadLine());
+            if(number % 2 == 0) {
+                Console.WriteLine("This is a even number");
+            }
+            else {
+                Console.WriteLine("This is a odd number");
+            }
+        }
+    }
+}
---
+++ Conditional Exercises/EC_Ex06.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EC_Ex06 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insert the vallue");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 0 && number < 25) {
+                Console.WriteLine("The number is in the range [0-25]");
+            }
+            else if (number > 25 && number < 50) {
+                Console.WriteLine("The number is in the range [25-50]");
+            }
+            else if (number > 50 && number < 75) {
+                Console.WriteLine("The number is in the range [50-75]");
+            }
+            else if (number > 75 && number < 100) {
+                Console.WriteLine("The number is in the range [75-100]");
+            }
+        }
+    }
+}
