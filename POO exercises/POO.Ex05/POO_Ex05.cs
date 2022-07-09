f30aa1c442ebd9a038faf8fa9b78d2931d41f82b
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-09 02:52:16 -0300

    Add files via upload

---
+++ POO exercises/POO.Ex05/POO_Ex05.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace POO_Ex05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Student student = new Student();
+            
+            Console.WriteLine("Insert the name of the student:");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Insert the first grade:");
+            student.Grade1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the second grade:");
+            student.Grade2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the third grade:");
+            student.Grade3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Final Grade: "+student.FinalGrade().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (student.Approval()) {
+                Console.WriteLine("Approved");
+            }
+            else {
+                Console.WriteLine("Disapproved");
+                Console.WriteLine("Remaining grade:"+student.RemainingGrade().ToString("F2", CultureInfo.InvariantCulture)+ "points.");
+            }
+        }
+    }
+}
---
+++ POO exercises/POO.Ex05/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace POO_Ex05 {
+    internal class Student {
+        public string Name;
+        public double Grade1;
+        public double Grade2;
+        public double Grade3;
+
+        public double FinalGrade() {
+            return Grade1 + Grade2 + Grade3;
+        }
+
+        public bool Approval() {
+            if(FinalGrade() >= 60) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public double RemainingGrade() {
+            if (Approval()) {
+                return 0.0;
+            }
+            else {
+                return 60.0 - FinalGrade();
+            }
+        }
+    }
+}
