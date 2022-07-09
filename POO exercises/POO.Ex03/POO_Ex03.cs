5c9e8205903dc33de2c81851855264b69e12b8c1
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-08 22:33:34 -0300

    Add files via upload

---
+++ POO exercises/POO.Ex03/POO_Ex03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+namespace POO_Ex03 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            Rectangle rectangle;
+            rectangle = new Rectangle();
+
+            Console.WriteLine("Insert the rectangle's width:");
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Insert the rectangle's height:");
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = " + rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimeter = " + rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ POO exercises/POO.Ex03/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace POO_Ex03 {
+    internal class Rectangle {
+        public double Height;
+        public double Width;
+
+        public double Area() {
+            return Height * Width;
+        }
+
+        public double Perimeter() {
+            return (2 * Height) + (2 * Width);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Height, 2) + Math.Pow(Width, 2));
+        }
+    }
+}
