047086ef6ca3a368796f758ddca35b2857229654
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-09 01:49:59 -0300

    Add files via upload

---
+++ POO exercises/POO.Ex04/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace POO_Ex04 {
+    internal class Employee {
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public double NetSalary() {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double percentage) {
+            GrossSalary += (GrossSalary * percentage/100);
+        }
+
+        public override string ToString() {
+            return "Employee: "+Name+", %"+NetSalary().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ POO exercises/POO.Ex04/POO_Ex04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POO_Ex04 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            Employee employee = new Employee();
+            double percentage;
+            
+            Console.WriteLine("Insert the employee's name:");
+            employee.Name = Console.ReadLine();
+            Console.WriteLine("Insert the employee's gross salary:");
+            employee.GrossSalary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the tax:");
+            employee.Tax = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(employee);
+
+            Console.WriteLine("Insert the percentage to increase the gross salary:");
+            percentage = double.Parse(Console.ReadLine());
+            employee.IncreaseSalary(percentage);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Updated data:{employee}");
+        }
+    }
+}
