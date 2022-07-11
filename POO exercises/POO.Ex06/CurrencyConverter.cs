18f565875d06eb45ef8e0d8043bd065f69d698c5
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-11 02:06:42 -0300

    Add files via upload

---
+++ POO exercises/POO.Ex06/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace POO_Ex06 {
+    internal class CurrencyConverter {
+
+        public static double IOF = 6.0; 
+        public static double ReaisPrice(double Dolar, double DolarExchange) {
+            return (Dolar * DolarExchange) + (Dolar * DolarExchange * IOF / 100);
+        }
+    }
+}
---
+++ POO exercises/POO.Ex06/POO_Ex06.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+namespace POO_Ex06 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double Dolar, DolarExchange;
+
+            Console.WriteLine("What's the dollar exchange rate?");
+            DolarExchange = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("How much dollar will you buy?");
+            Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double ReaisPrice = CurrencyConverter.ReaisPrice(Dolar, DolarExchange);
+            Console.WriteLine("Amount to be paid in reais:"+ReaisPrice.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
