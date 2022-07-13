ebb7e4bbd820e1f33e09e99d899eca1323046730
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-12 21:48:19 -0300

    Improved Exercises(Ex.ESPECIAL)

---
+++ POO exercises/POO.ExEspecialImproved/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace POO_ExESPECIAL {
+    internal class BankAccount {
+
+        private string _name;
+        public int Account { get; private set; }
+        public double DepositMoney { get; private set; }
+
+        public BankAccount() {
+        }
+        public BankAccount(int account, string name) {
+            Account = account;
+            _name = name;
+            DepositMoney = 0;
+        }
+
+        public BankAccount(int account, string name, double depositMoney) : this(account, name) {
+            Deposit(depositMoney);
+        }
+
+        public string Name {
+            get { return _name; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _name = value;
+                }
+            }
+        }
+
+        public void Deposit(double money) {
+            DepositMoney += money;
+        }
+
+        public void WithdrawMoney(double money) {
+             DepositMoney -= money;
+            DepositMoney -= 5.0; //tax
+        }
+
+        override public string ToString() {
+            return "Account:" + Account +
+                ", Account owner:" + _name + 
+                ", balance: $" + DepositMoney.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ POO exercises/POO.ExEspecialImproved/POO_ExESPECIAL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace POO_ExESPECIAL {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int account;
+            string name;
+            char depositMoney;
+            double money;
+
+            BankAccount bankAccount;
+
+            Console.WriteLine("Insert the account number:");
+            account = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert the account owner:");
+            name = Console.ReadLine();
+            Console.WriteLine("Will there be an initial deposit? (s/n)");
+            depositMoney = char.Parse(Console.ReadLine());
+            while(depositMoney != 's' && depositMoney != 'n') {
+                Console.WriteLine("Will there be an initial deposit? (s/n)");
+                depositMoney = char.Parse(Console.ReadLine());
+            }
+            if (depositMoney == 's' || depositMoney == 'S') {
+                Console.WriteLine("How much is your deposit?");
+                money = double.Parse(Console.ReadLine());
+                bankAccount = new BankAccount(account, name, money);
+            }
+            else {
+                bankAccount = new BankAccount(account, name);
+            }
+
+            Console.WriteLine(bankAccount);
+            Console.WriteLine("Insert a vallue to deposit money:");
+            money = double.Parse(Console.ReadLine());
+            bankAccount.Deposit(money);
+            Console.WriteLine("Bank account updated:" + bankAccount);
+            Console.WriteLine("Insert a vallue to withdraw money:");
+            money = double.Parse(Console.ReadLine());
+            bankAccount.WithdrawMoney(money);
+            Console.WriteLine("Bank account updated:");
+            Console.WriteLine(bankAccount);
+        }
+    }
+}
