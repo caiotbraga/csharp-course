a20d727d6a7041dd9c94fe1a38d15f57fd19608b
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-14 22:52:47 -0300

    Add files via upload

---
+++ Vector exercises/Vector.Ex01/Room.cs
@@ -0,0 +1,19 @@
+﻿ namespace EV_EX01 {
+    internal class Room {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Number { get; set; }
+
+        /* public Room(string name, string email, int number) {
+             Name = name;
+             Email = email;
+             Number = number;
+         }*/
+        override public string ToString() {
+            return Name+", " + Email;
+        }
+    }
+
+
+}
