7d4202b52b323e7247c3baeb83fe7fc41c7c29cb
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-16 19:10:25 -0300

    Add files via upload

---
+++ List exercises/List.Ex01/List_Ex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+namespace List_Ex {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n, id, i, searchId;
+            string name;
+            double salary, percentage;
+            
+            Console.WriteLine("How many peoples will be registred?");
+            n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+            for (i = 0; i < n; i++) {
+                Console.WriteLine("Employee "+(i+1)+":");
+                Console.WriteLine("ID:");
+                id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Name:");
+                name = Console.ReadLine();
+                Console.WriteLine("Salary:");
+                salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary)); // instanciando o objeto na lista
+                Console.WriteLine();
+            }
+            Console.WriteLine("Enter the employee id that will have salary increase :");
+            searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchId); // criando a variavel objeto 
+            if(emp != null) {
+                Console.WriteLine("Enter the percentage:");
+                percentage = double.Parse(Console.ReadLine());
+                emp.IncreaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id does not exist!");
+            }
+            Console.WriteLine("Updated list of employees:");
+            foreach(Employee employee in list) {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
