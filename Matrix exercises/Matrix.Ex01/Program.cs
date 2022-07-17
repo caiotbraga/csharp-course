0f7b8a31e0274d0541e2755e7bcf897c0f716cfc
caiotbraga <108440048+caiotbraga@users.noreply.github.com>
2022-07-17 18:30:21 -0300

    Add files via upload

---
+++ Matrix exercises/Matrix.Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mat_Ex01 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            int n, i, j, countN = 0;
+            int v;
+            Console.WriteLine("Enter the matrix's order:");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for(i = 0; i < n; i++) {
+                string[] vallue = Console.ReadLine().Split(' ');
+                for (j = 0; j < n; j++) {
+                    matrix[i,j] = int.Parse(vallue[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal:");
+            for(i = 0; i < n; i++) {
+                Console.WriteLine(matrix[i,i]+" ");
+            }
+
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < n; j++) {
+                    if (matrix[i,j] < 0) {
+                        countN++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Negative numbers: {countN}");
+
+
+        }
+    }
+}
