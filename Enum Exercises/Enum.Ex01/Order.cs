2e80e09fe236b365cc0c8674e5c958d913ced3ee
Caio Braga <108440048+caiotbraga@users.noreply.github.com>
2022-07-20 23:13:39 -0300

    Add files via upload

---
+++ Enum Exercises/Enum.Ex01/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Enum_Ex01.Entities.Enums;
+
+namespace Enum_Ex01.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return "Id:" 
+                + Id
+                + ","
+                + Moment
+                + ","
+                + Status;
+        }
+    }
+}
---
+++ Enum Exercises/Enum.Ex01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Enum_Ex01.Entities;
+using Enum_Ex01.Entities.Enums;
+namespace Enum_Ex01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
