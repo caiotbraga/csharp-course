e410778f1534682d7a2a6949607ba2a42d6292e3
Caio Braga <108440048+caiotbraga@users.noreply.github.com>
2022-07-23 02:38:09 -0300

    Fixation Exercise

---
+++ Enum Exercises/Enum.Ex04/Order.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Enum_Ex04.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+namespace Enum_Ex04.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Clients Client { get; set; }
+        List<OrderItem> ListItems { get; set; } = new List<OrderItem>();
+
+        public Order() { 
+        }
+        public Order(DateTime moment, OrderStatus status, Clients client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem Item)
+        {
+            ListItems.Add(Item);
+        }
+
+        public void RemoveItem(OrderItem Item)
+        {
+            ListItems.Remove(Item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem Item in ListItems)
+            {
+                
+                sum += Item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: "+ Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in ListItems)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Enum Exercises/Enum.Ex04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Enum_Ex04.Entities.Enums;
+using Enum_Ex04.Entities;
+using System.Globalization;
+namespace Enum_Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name:"); string clientName = Console.ReadLine();
+            Console.Write("Email:"); string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY)"); DateTime birthDay = DateTime.Parse(Console.ReadLine());
+
+            Clients client = new Clients(clientName, email, birthDay);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status:"); OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order?"); int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data:");
+                Console.Write("Product name:"); string nameP = Console.ReadLine();
+                Console.Write("Product price:"); double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(nameP, price);    
+                Console.Write("Quantity:"); int quantity = int.Parse(Console.ReadLine());
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
