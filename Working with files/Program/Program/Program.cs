94d5cead13f9cb69a15dc26ea99ad01c44f899bd
Caio Braga <caiotabraga@hotmail.com>
2022-10-28 13:50:04 -0300

    File, FileInfo and IOException

---
+++ Working with files/Program/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt"; //@ serve pra nao precisar usar 2 barras (\\)
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
