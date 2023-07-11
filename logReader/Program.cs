2525d400ee1a7fa71f3725555773c0212287515e
Caio Braga <108440048+caiotbraga@users.noreply.github.com>
2023-07-11 15:08:03 -0300

    Add files via upload

---
+++ logReader/Program.cs
@@ -0,0 +1,21 @@
+﻿using logReader.entidades;
+
+HashSet<logRecord> set = new HashSet<logRecord>();
+Console.Write("Enter full path: ");
+String path = Console.ReadLine();
+
+try {
+	using (StreamReader sr = File.OpenText(path)) {
+		while (!sr.EndOfStream) {
+			string[] line = sr.ReadLine().Split(' ');
+			string name = line[0];
+			DateTime moment = DateTime.Parse(line[1]);
+			set.Add(new logRecord(name, moment));
+		}
+        Console.WriteLine("Total users: "+set.Count);
+    }
+}
+catch (Exception) {
+
+	throw;
+}
---
+++ logReader/entidades/logRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logReader.entidades {
+    internal class logRecord {
+        public String Username { get; set; }
+        private DateTime moment;
+
+        public logRecord(string name, DateTime dateTime) {
+            this.Username = name;
+            this.moment = dateTime;
+        }
+
+        public override int GetHashCode() {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object? obj) {
+            if(!(obj is logRecord)) {
+                return false;
+            }
+            logRecord other = obj as logRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
