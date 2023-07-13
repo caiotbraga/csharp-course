952e2dd74ab266ce9dee0a2ef40a43ea2abde191
Caio Braga <108440048+caiotbraga@users.noreply.github.com>
2023-07-13 15:55:07 -0300

    HashSetExercise

---
+++ HashSetExercise/Entities/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetExercise.Entities {
+    internal class Students {
+        public Students(int id) {
+            this.id = id;
+        }
+
+        public int id { get; set; }
+
+        public override int GetHashCode() {
+            return id.GetHashCode();
+        }
+
+        public override bool Equals(object? obj) {
+            if(!(obj is Students)) {
+                return false;
+            }
+            Students other = obj as Students; 
+            return id.Equals(other.id);
+        }
+    }
+}
---
+++ HashSetExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using HashSetExercise.Entities;
+
+HashSet<Students> set = new HashSet<Students> ();
+Console.WriteLine("How many Students for course A?");
+int nA = int.Parse (Console.ReadLine ());
+for (int i = 0; i < nA; i++) {
+    int number = int.Parse (Console.ReadLine ());
+    set.Add(new Students (number));
+}
+Console.WriteLine("How many Students for course B?");
+int nB = int.Parse(Console.ReadLine());
+for (int i = 0; i < nB; i++) {
+    int number = int.Parse(Console.ReadLine());
+    set.Add(new Students(number));
+}
+Console.WriteLine("How many Students for course C?");
+int nC = int.Parse(Console.ReadLine());
+for (int i = 0; i < nC; i++) {
+    int number = int.Parse(Console.ReadLine());
+    set.Add(new Students(number));
+}
+Console.WriteLine("Total Students: "+set.Count);
+
