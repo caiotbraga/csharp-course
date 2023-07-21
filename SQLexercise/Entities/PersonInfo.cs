a7829ab0088e969cd83846f2ff56fddf295ce288
Caio Braga <108440048+caiotbraga@users.noreply.github.com>
2023-07-21 16:10:43 -0300

    SQLexercise with lambda

---
+++ SQLexercise/Entities/PersonInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLexercise.Entities
+{
+    internal class PersonInfo 
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public double salary { get; set; }
+
+        public PersonInfo(string name, string email, double salary)
+        {
+            this.name = name;
+            this.email = email;
+            this.salary = salary;
+        }
+    }
+}
---
+++ SQLexercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using SQLexercise.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+try
+{
+    Console.Write("Write the path: ");
+    String path = Console.ReadLine();
+    Console.WriteLine("Insert salary: ");
+    Double salary = double.Parse(Console.ReadLine());
+
+    List<PersonInfo> list = new List<PersonInfo>();
+
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            String[] text = sr.ReadLine().Split(", ");
+            String names = text[0];
+            String emails = text[1];
+            Double salarys = Double.Parse(text[2], CultureInfo.InvariantCulture);
+
+            list.Add(new PersonInfo(names, emails, salarys));
+        }
+    }
+    var moreThan2000 = list.Where(obj => obj.salary > salary).OrderBy(obj => obj.email).Select(obj => obj.email);
+    //var moreThan2000 = list.Where(pi => pi.salary > salary).OrderBy(email => email).ToList();
+    Console.WriteLine("Emails of people whose have more than " + salary + ": ");
+    foreach (var item in moreThan2000)
+    {
+        Console.WriteLine(item);
+    }
+    Console.Write("Sum of salary of people whose names start with 'M': ");
+    var sum = list.Where(pi => pi.name.StartsWith('M')).Sum(obj => obj.salary);
+    Console.WriteLine(sum.ToString("F2", CultureInfo.InvariantCulture));
+}
+catch (Exception)
+{
+
+    throw;
+}
